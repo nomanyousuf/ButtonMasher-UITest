f8644e99f50e0a23524564a0e51e1e50470661c4
Noman Yousuf <nomanyousuf@Zeeshans-MacBook-Pro.local>
2019-11-27 12:15:08 -0500

    adding a label

---
+++ UITest/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by Noman Yousuf on 11/27/19.
+//  Copyright © 2019 colors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var  UIViewMain: UIView!
+    @IBOutlet weak var  button: UIButton!
+    var myLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+        func getRandomColor() -> UIColor {
+             let r:CGFloat  = .random(in: 0...1)
+             let g:CGFloat  = .random(in: 0...1)
+             let b:CGFloat  = .random(in: 0...1)
+             return UIColor(red: r, green: g, blue: b, alpha: 1)
+        }
+    
+
+    @IBAction func myButton(_ sender: Any){
+        UIViewMain.backgroundColor = getRandomColor()
+        myLabel = UILabel(frame: CGRect(x: UIScreen.main.bounds.midX - 100, y: 200, width: 200, height: 30))
+        myLabel.text = "Welcome to school"
+        myLabel.textColor = .green
+        view.addSubview(myLabel)
+    }
+    
+    
+}
+
---
+++ UITestUITests/UITestUITests.swift
@@ -0,0 +1,41 @@
+//
+//  UITestUITests.swift
+//  UITestUITests
+//
+//  Created by Noman Yousuf on 11/27/19.
+//  Copyright © 2019 colors. All rights reserved.
+//
+
+import XCTest
+
+class UITestUITests: XCTestCase {
+    
+    var app : XCUIApplication!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
+       
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+        super.tearDown()
+    }
+
+    func testChangeColor_ButtonPressed_ShouldCreateAlabel(){
+        app.buttons["Get colors"].tap()
+        XCTAssertTrue(app.staticTexts["Welcome to school"].exists)
+    }
+    
+    
+  
+}
